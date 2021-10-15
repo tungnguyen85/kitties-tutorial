f58a2f4d783ab35561e1eb29c15485304c54eda6
Tung Nguyen <tungnguyen@os.amperecomputing.com>
2021-10-15 10:04:38 +0000

    done: dispatches, event, error handling
    Signed-off-by: Tung Nguyen <tungnguyen@os.amperecomputing.com>

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -71,6 +71,24 @@ pub mod pallet {
 	#[pallet::error]
 	pub enum Error<T> {
 		// ACTION #5a: Declare errors.
+        /// Handles arithemtic overflow when incrementing the Kitty counter.
+        KittyCntOverflow,
+        /// An account cannot own more Kitties than `MaxKittyCount`.
+        ExceedMaxKittyOwned,
+        /// Buyer cannot be the owner.
+        BuyerIsKittyOwner,
+        /// Cannot transfer a kitty to its owner.
+        TransferToSelf,
+        /// Handles checking whether the Kitty exists.
+        KittyNotExist,
+        /// Handles checking that the Kitty is owned by the account transferring, buying or setting a price for it.
+        NotKittyOwner,
+        /// Ensures the Kitty is for sale.
+        KittyNotForSale,
+        /// Ensures that the buying price is greater than the asking price.
+        KittyBidPriceTooLow,
+        /// Ensures that an account has enough funds to purchase a Kitty.
+        NotEnoughBalance,
 	}
 
 	// Events.
@@ -78,6 +96,14 @@ pub mod pallet {
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
 		// ACTION #3: Declare events
+        /// A new Kitty was sucessfully created. \[sender, kitty_id\]
+        Created(T::AccountId, T::Hash),
+        /// Kitty price was sucessfully set. \[sender, kitty_id, new_price\]
+        PriceSet(T::AccountId, T::Hash, Option<BalanceOf<T>>),
+        /// A Kitty was sucessfully transferred. \[from, to, kitty_id\]
+        Transferred(T::AccountId, T::AccountId, T::Hash),
+        /// A Kitty was sucessfully bought. \[buyer, seller, kitty_id, bid_price\]
+        Bought(T::AccountId, T::AccountId, T::Hash, BalanceOf<T>),
 	}
 
 	// Storage items.
@@ -130,11 +156,19 @@ pub mod pallet {
 		#[pallet::weight(100)]
 		pub fn create_kitty(origin: OriginFor<T>) -> DispatchResult {
 			// ACTION #1: create_kitty
+            let sender = ensure_signed(origin)?;
+
+            let kitty_id = Self::mint(&sender, None, None)?;
+        
+            // Logging to the console
+            log::info!("A kitty is born with ID: {:?}.", kitty_id);
 
 			// ACTION #4: Deposit `Created` event
+            Self::deposit_event(Event::Created(sender, kitty_id));
 
 			Ok(())
 		}
+    }
 
 		// TODO Part IV: set_price
 
@@ -143,7 +177,6 @@ pub mod pallet {
 		// TODO Part IV: buy_kitty
 
 		// TODO Part IV: breed_kitty
-	}
 
 	//** Our helper functions.**//
 
@@ -169,6 +202,34 @@ pub mod pallet {
 		// Create new DNA with existing DNA
 
 		// ACTION #2: Write mint function
+        // Helper to mint a Kitty.
+        pub fn mint(
+            owner: &T::AccountId,
+            dna: Option<[u8; 16]>,
+            gender: Option<Gender>,
+        ) -> Result<T::Hash, Error<T>> {
+            let kitty = Kitty::<T> {
+            dna: dna.unwrap_or_else(Self::gen_dna),
+            price: None,
+            gender: gender.unwrap_or_else(Self::gen_gender),
+            owner: owner.clone(),
+            };
+        
+            let kitty_id = T::Hashing::hash_of(&kitty);
+        
+            // Performs this operation first as it may fail
+            let new_cnt = Self::kitty_cnt().checked_add(1)
+            .ok_or(<Error<T>>::KittyCntOverflow)?;
+        
+            // Performs this operation first because as it may fail
+            <KittiesOwned<T>>::try_mutate(&owner, |kitty_vec| {
+            kitty_vec.try_push(kitty_id)
+            }).map_err(|_| <Error<T>>::ExceedMaxKittyOwned)?;
+        
+            <Kitties<T>>::insert(kitty_id, kitty);
+            <KittyCnt<T>>::put(new_cnt);
+            Ok(kitty_id)
+        }
 
 		// Helper to check correct kitty owner
 
